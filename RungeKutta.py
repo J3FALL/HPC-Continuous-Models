1e9dd18d15fb2ca5f7945b09fd67f933752742a9
J3FALL <crisp.coad@gmail.com>
2017-09-24 23:32:02 +0300

    Add Runge-Kutta method

---
+++ RungeKutta.py
@@ -0,0 +1,52 @@
+from math import pow
+from math import tan
+
+import plotly.figure_factory as ff
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+
+def runge_kutta():
+    h = 0.1
+    x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    y_eval = [0.0]
+    delta_y = [0.0]
+    y_real = [0.0]
+    eps = [0.0]
+
+    for i in range(1, len(x)):
+        k1 = h * fun(x[i - 1], y_eval[i - 1])
+        k2 = h * fun(x[i - 1] + 0.5 * h, y_eval[i - 1] + 0.5 * k1)
+        k3 = h * fun(x[i - 1] + 0.5 * h, y_eval[i - 1] + 0.5 * k2)
+        k4 = h * fun(x[i - 1] + h, y_eval[i - 1] + k3)
+
+        d = (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+        y_eval.append(y_eval[i - 1] + d)
+
+        delta_y.append(h * fun(y_eval[i], x[i]))
+        y_real.append(real_fun(x[i]))
+        eps.append(abs(y_real[i] - y_eval[i]))
+
+    # print in table format
+    print(y_eval)
+    print(delta_y)
+    print(y_real)
+    print(eps)
+
+    data_matrix = [
+        ['k', 'x', 'y', 'delta_y', 'y_real', 'eps']
+    ]
+    for i in range(0, len(x)):
+        data_matrix.append([i, x[i], y_eval[i], delta_y[i], y_real[i], eps[i]])
+
+    table = ff.create_table(data_matrix)
+    plot(table)
+
+
+def fun(x, y):
+    return pow(x + y, 2)
+
+
+def real_fun(x):
+    return tan(x) - x
+
+runge_kutta()
